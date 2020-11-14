7277937ff6c02fc547957afdb1b1b68924cfbe2a
Carlos Silva <carlos.vitor.caetano@gmail.com>
2020-11-14 14:29:46 -0300

    solicitando acces token do spotify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import $ from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    const parametros = this.getHashParams();
+    this.token = parametros.access_token;
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    console.log(this.token);
+    return hashParams;
+  }
+
+  topTracks = () =>{
+    $.ajax({
+      method: "GET", 
+      dataType: "Json",
+      url: "https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=BR",
+      headers: {        
+        Authorization: `Bearer ${this.token}`
+      }
+    })
+    .then(dados => {
+      console.log(dados.tracks[0].name);
+      console.log(this.topTracks);
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button><a href="http://localhost:8888/"> Logar com Spotify </a></button>
+        <button onClick={this.topTracks}> Buscar top tracks </button>
+      </div>
+    );
+  }
 }
 
 export default App;
